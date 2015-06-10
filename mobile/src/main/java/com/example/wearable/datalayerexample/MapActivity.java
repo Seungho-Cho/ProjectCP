80cf8eb4de52e4cb6163b26c0f7f9e8392c7ae7f
csh <csh628@gmail.com>
2015-06-10 18:05:22 +0900

    MapActivity 제스처 어댑터 추가

--- mobile/src/main/java/com/example/wearable/datalayerexample/MapActivity.java
+++ mobile/src/main/java/com/example/wearable/datalayerexample/MapActivity.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,7 +33,10 @@ import org.w3c.dom.Text;
 import java.util.LinkedList;
 
 
-public class MapActivity extends NMapActivity {
+public class MapActivity extends NMapActivity
+        implements GestureDetector.OnGestureListener
+
+{
 
     private static final String LOG_TAG = "MapActivity";
     private static final boolean DEBUG = false;
@@ -51,6 +56,19 @@ public class MapActivity extends NMapActivity {
 
     private TTSAdapter tts;
     private Intent mIntent;
+
+
+    private static final int SWIPE_MIN_DISTANCE = 120;          // 제스처 최소 거리
+    private static final int SWIPE_MAX_OFF_PATH = 2000;         // 제스처 최대 거리
+    private static final int SWIPE_THRESHOLD_VELOCITY = 50;    // 제스처 인식 속도
+    private GestureDetector gestureDetector;    // 제스처 처리
+
+    private long tap_time = 0;
+    private int tap_count = 0;
+    private int tap_on = 0;
+    private int long_press_on = 0;
+
+    LinearLayout main_Layout;
 
     GNode[] GNodeArr = {
             new GNode(0, 0, null, null, 126.7320299, 37.3422762),
@@ -122,8 +140,17 @@ public class MapActivity extends NMapActivity {
     double[] location = new double[2];
     int destination = 0;
 
-    ////////////////////////////////////////////////////
-    // test
+    /////////////////////  제스처 디텍터 ///////////////////////////////
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        Log.d("onTouch","onTouch");
+        return gestureDetector.onTouchEvent(event) || super.onTouchEvent(event);
+    }
+
+
+
+    ///////////////////////////////////////////////////
 
     TextView lonText, latText, radText;
 
@@ -134,9 +161,22 @@ public class MapActivity extends NMapActivity {
         setContentView(R.layout.activity_map);
 
         tts = new TTSAdapter(this);
+        gestureDetector = new GestureDetector(this);
 
         // create map view
         mMapView = (NMapView)findViewById(R.id.mapView);
+        main_Layout = (LinearLayout) findViewById(R.id.mapLayout);
+
+        main_Layout.setOnTouchListener( new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                gestureDetector.onTouchEvent(event);
+                return true;
+            }
+        });
+
         //mMapView = new NMapView(this);
 
         // set a registered API key for Open MapViewer Library
@@ -753,5 +793,181 @@ public class MapActivity extends NMapActivity {
                 }
             }
         }
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e)
+    {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e)
+    {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e)
+    {
+        if(tap_count==2)
+        {
+            Log.d("tap","triple");
+            if(System.currentTimeMillis() <= tap_time + 2000)
+            {
+                tap_triple();
+                tap_count=0;
+                tap_on = 0;
+                return true;
+            }
+        }
+        if(tap_count==1)
+        {
+            Log.d("tap","double");
+            tap_count=2;
+            if(System.currentTimeMillis() <= tap_time + 2000)
+            {
+                Thread thread = new Thread()
+                {
+                    public void run()
+                    {
+                        tap_on = 2;
+                        try
+                        {
+                            Thread.sleep(2000);
+                            if(tap_on==2)
+                            {
+                                tap_double();
+                                tap_count=0;
+                            }
+
+                        } catch (InterruptedException e1)
+                        {
+                            e1.printStackTrace();
+                        }
+                    }
+                };
+                thread.start();
+                return true;
+            }
+        }
+        if(tap_count==0)
+        {
+            Log.d("tap","single");
+            if (System.currentTimeMillis() > tap_time + 2000)
+            {
+                tap_time = System.currentTimeMillis();
+                tap_count=1;
+
+                Thread thread = new Thread()
+                {
+                    public void run()
+                    {
+                        tap_on = 1;
+                        try
+                        {
+                            Thread.sleep(2000);
+                            if(tap_on==1)
+                            {
+                                tap_single();
+                                tap_count=0;
+                            }
+
+                        } catch (InterruptedException e1)
+                        {
+                            e1.printStackTrace();
+                        }
+                    }
+                };
+                thread.start();
+                return true;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY)
+    {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e)
+    {
+        //cp_TTS("테스트");
+        Log.d("tap","Long");
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY)
+    {
+        try
+        {
+            if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
+                return false;
+
+            // right to left swipe
+            if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY)
+            {
+                Toast.makeText(this, "Left Swipe", Toast.LENGTH_SHORT).show();
+                swipe_left();
+            }
+            // left to right swipe
+            else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY)
+            {
+                Toast.makeText(this, "Right Swipe", Toast.LENGTH_SHORT).show();
+                swipe_right();
+            }
+            // down to up swipe
+            else if (e1.getY() - e2.getY() > SWIPE_MIN_DISTANCE && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY)
+            {
+                Toast.makeText(this, "Swipe up", Toast.LENGTH_SHORT).show();
+                swipe_up();
+            }
+            // up to down swipe
+            else if (e2.getY() - e1.getY() > SWIPE_MIN_DISTANCE && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY)
+            {
+                Toast.makeText(this, "Swipe down", Toast.LENGTH_SHORT).show();
+                swipe_down();
+            }
+        } catch (Exception e) {  }
+        return true;
+    }
+
+
+    public void swipe_left()
+    {
+
+    }
+
+    public void swipe_right()
+    {
+
+    }
+
+    public void swipe_up()
+    {
+
+    }
+
+    public void swipe_down()
+    {
+
+    }
+
+    public void tap_single()
+    {
+
+    }
+
+    public void tap_double()
+    {
+
+    }
+
+    public void tap_triple()
+    {
+
     }
 }
